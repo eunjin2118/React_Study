05a6c5a3eb2136ad71d1bcc2c9dc9049ba72face
은진 <80873499+eunjin2118@users.noreply.github.com>
2023-02-21 23:55:55 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 import Box from "./component/Box"
 
 function App() {
+  let counter = 0;
+  const [counter2, setCounter2] = useState(0);
+  const increase = () => {
+    counter = counter + 1;
+    setCounter2(counter2+1);
+    console.log(counter);
+  }
   return (
     <div>
-      <Box name="리사" num="1"/>
-      <Box name="제니" num="2"/>
-      <Box name="지수" num="3"/>
+      <div>{counter}</div>
+      <div>state:{counter2}</div>
+      <button onClick={increase}>클릭!</button>
     </div>
   );
 }
